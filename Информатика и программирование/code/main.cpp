cfd1240276343391da874399c8fe23f693b101e6
krypton1101 <krypton11@mail.ru>
2023-04-21 14:12:14 +0300

    vault backup: 2023-04-21 14:13:00

--- Информатика и программирование/code/main.cpp
+++ Информатика и программирование/code/main.cpp
@@ -1,68 +1,155 @@
 #include <iostream>
-using namespace std;
 
-#define SIZE 10
-class stack
+int twoPower(unsigned int n)
 {
-	char *stck;
-	int top, size;
-public:
-	stack(int s);
-	~stack();
-	void push(char ch);
-	char pop();
-};
-
-stack::stack(int s)
-{
-	cout << "Constructor works" << endl;
-	top = 0;
-	stck = new char[s];
-	size = s;
-	if(!stck)
+	int out = 1;
+	for(int i=0; i<n; i++)
 	{
-		cout << "Segfault" << endl;
-		exit(1);
+		out*=2;
+	}
+	return out;
+}
+void printNet(char net, int one)
+{
+	switch(net)
+	{
+		case 'A':
+		{
+			int dots=1;
+			std::cout << "11111111.";
+			while(one>=8)
+			{
+				one-=8;
+				std::cout << "11111111.";
+				dots++;
+			}
+			for(int i=0;i<one;i++)
+			{
+				std::cout<<"1";
+			}
+			for(int i=0;i<8-one;i++)
+			{
+				std::cout<<"0";
+			}
+			while(dots<3)
+			{
+				std::cout << ".00000000";
+				dots++;
+			}
+			std::cout << std::endl;
+			if(one<8) std::cout << "0." << twoPower(8-one) << ".0.1" << std::endl;
+			else if(one<16) std::cout << "0.0." << twoPower(16-one) << ".1" << std::endl;
+			else std::cout << "0.0.0." << twoPower(24-one)+1 << std::endl;
+
+			if(one<8) std::cout << "127." << 255-twoPower(8-one) << "255.254" << std::endl;
+			else if(one>16) std::cout << "127.255." << 255-twoPower(16-one) << ".254" << std::endl;
+			else std::cout << "127.255.255." << 254-twoPower(24-one) << std::endl;
+			break;
+		}
+		case 'B':
+		{
+			int dots=2;
+			std::cout << "11111111.11111111.";
+			while(one>=8)
+			{
+				one-=8;
+				std::cout << "11111111.";
+				dots++;
+			}
+			for(int i=0;i<one;i++)
+			{
+				std::cout<<"1";
+			}
+			for(int i=0;i<8-one;i++)
+			{
+				std::cout<<"0";
+			}
+			while(dots<3)
+			{
+				std::cout << ".00000000";
+				dots++;
+			}
+			std::cout << std::endl;
+			if(one<8) std::cout << "128.0." << twoPower(8-one) << ".1" << std::endl;
+			else std::cout << "128.0.0." << twoPower(16-one)+1 << std::endl;
+
+			if(one<16) std::cout << "191.255." << 255-twoPower(8-one) << ".254" << std::endl;
+			else std::cout << "191.255.255." << 254-twoPower(16-one) << std::endl;
+			break;
+		}
+		case 'C':
+		{
+			std::cout << "11111111.11111111.11111111.";
+			for(int i=0;i<one;i++)
+			{
+				std::cout<<"1";
+			}
+			for(int i=0;i<8-one;i++)
+			{
+				std::cout<<"0";
+			}
+			std::cout << std::endl;
+			std::cout << "192.0.0." << 1+twoPower(8-one) << std::endl;
+			std::cout << "223.255.255." << 254-twoPower(8-one) << std::endl;
+			break;
+		}
 	}
 }
-stack::~stack()
+int powerof2(int n)
 {
-	cout << "Destuctor works" << endl;
-	delete stck; //delete *stck; //delete stck[]; //delete []stck;
-}
-void stack::push(char ch)
-{
-	if(top==SIZE)
+	n+=2;
+	int two=1;
+	for(unsigned int i=0;i<24;i++)
 	{
-		cout << "Stack is full" << endl;
-		return;
+		if(n-two<=0)
+		{
+			return i;
+		}
+		two*=2;
 	}
-	stck[top] = ch;
-	top++;
+	return -1;
 }
-char stack::pop()
-{
-	if(!top)
-	{
-		cout << "Stack is empty" << endl;
-		return 0;
-	}
-	//
-	top--;
-	return stck[top];
-}
-
 
 int main()
 {
-	stack s1,s2;
-	int i;
-	s1.push('a');
-	s2.push('x');
-	s1.push('g');
-	s2.push('v');
-	s1.push('h');
-	s2.push('l');
-
-	cout << s1.pop() << " " << s2.pop() << s2.pop() << s2.pop() << s2.pop() << endl;
+    char NetClass;
+    int subnetCount, machineCount;
+    std::cout << "enter net class(A,B or C): ";
+    std::cin >> NetClass;
+    std::cout << "enter subnet quantity: ";
+    std::cin >> subnetCount;
+    std::cout << "enter max computer quantity: ";
+    std::cin >> machineCount;
+    
+	int free;
+	switch(NetClass)
+	{
+		case 'A':
+		{
+			free = 24;
+			break;
+		}
+		case 'B':
+		{
+			free = 16;
+			break;
+		}
+		case 'C':
+		{
+			free = 8;
+			break;
+		}
+		default:
+		{
+			std::cout << "Incorrect net class." << std::endl;
+			return 0;
+		}
+	}
+	if(twoPower(free-powerof2(subnetCount))<machineCount)
+	{
+		std::cout << "Unable to set this configuration of subnets." << std::endl;
+		return 0;
+	}
+	printNet(NetClass, powerof2(subnetCount));
+	return 0;
 }
